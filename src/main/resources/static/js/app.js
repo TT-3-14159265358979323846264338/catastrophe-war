9b1b276a3683fdd9611ab6d46efad6f843dad09a
T.T. <196872942+TT-3-14159265358979323846264338@users.noreply.github.com>
2026-03-04 18:12:43 +0900

    Add files via upload

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -14,23 +14,25 @@ stompClient.connect({}, () => {
 			image.src = list[i];
 			coreImages.push(image);
 		}
+		stompClient.send("/app/timerStart", {}, {});
 	});
 	stompClient.subscribe('/topic/topPage', (load) => drawImage(JSON.parse(load.body).state));
 	stompClient.send("/app/requestImages", {}, {});
 });
 
 function drawImage(state) {
-	console.log("受信データ:", state);
 	if(coreImages.length === 0){
 		return;
 	}
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
 	state.forEach(i => {
 		const image = coreImages[i.id];
+		const width = image.width / 2
+		const height = image.height / 2;
 		ctx.save();
-		ctx.translate(i.x, i.y);
+		ctx.translate(i.x + width, i.y + height);
 		ctx.rotate(i.angle);
-		ctx.drawImage(image, -image.width / 2, -image.height / 2);
+		ctx.drawImage(image, -width, -height);
 		ctx.restore();
 	});
 }
