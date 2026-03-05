cb684f90e2ab0daadc4ccf516956cc8d0164905f
T.T. <196872942+TT-3-14159265358979323846264338@users.noreply.github.com>
2026-03-05 12:44:34 +0900

    Add files via upload

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -5,39 +5,70 @@ const stompClient = Stomp.over(socket);
 const titleImage = new Image();
 const coreImages = [];
 const TITLE_X = 80;
-const TITLE_Y = 100;
+const TITLE_Y = 40;
 
 stompClient.connect({}, () => {
 	stompClient.send("/app/top/images", {}, {});
-	stompClient.subscribe("/topic/top/images", (load) => {
-		const list = JSON.parse(load.body);
-		titleImage.src = list[0];
-		for(let i = 1; i < list.length; i++){
-			const image = new Image();
-			image.src = list[i];
-			coreImages.push(image);
-		}
-		stompClient.send("/app/top/timer/start", {}, {});
-	});
-	stompClient.subscribe('/topic/top/repaint', (load) => drawImage(JSON.parse(load.body)));
+	stompClient.subscribe("/topic/top/images", inputImage);
+	stompClient.subscribe('/topic/top/repaint', drawImage);
 });
 
+function inputImage(data){
+	const imageList = JSON.parse(data.body);
+	titleImage.src = imageList[0];
+	for(let i = 1; i < imageList.length; i++){
+		const image = new Image();
+		image.src = imageList[i];
+		coreImages.push(image);
+	}
+	stompClient.send("/app/top/timer/start", {}, {});
+}
+
 function drawImage(data) {
-	const {state, isEnded} = data;
+	const {state, isEnded} = JSON.parse(data.body);
 	ctx.clearRect(0, 0, canvas.width, canvas.height);
-	state.forEach(i => {
-		const image = coreImages[i.id];
-		const width = image.width / 2
-		const height = image.height / 2;
-		ctx.save();
-		ctx.translate(i.x + width, i.y + height);
-		ctx.rotate(i.angle);
-		ctx.drawImage(image, -width, -height);
-		ctx.restore();
-	});
+	state.forEach(draw);
 	if(isEnded){
 		ctx.drawImage(titleImage, TITLE_X, TITLE_Y);
 	}
+}
+
+function draw(coreState){
+	const image = coreImages[coreState.id];
+	const width = image.width / 2
+	const height = image.height / 2;
+	ctx.save();
+	ctx.translate(coreState.x + width, coreState.y + height);
+	ctx.rotate(coreState.angle);
+	ctx.drawImage(image, -width, -height);
+	ctx.restore();
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+	const gachaButton = document.getElementById("gacha");
+	const recycleButton = document.getElementById("recycle");
+	const compositionButton = document.getElementById("composition");
+	const stageButton = document.getElementById("stage");
+	gachaButton.addEventListener('click', gachaButtonAction);
+	recycleButton.addEventListener('click', recycleButtonAction);
+	compositionButton.addEventListener('click', compositionButtonAction);
+	stageButton.addEventListener('click', stageButtonAction);
+});
+
+function gachaButtonAction(_){
+	
+}
+
+function recycleButtonAction(_){
+	
+}
+
+function compositionButtonAction(_){
+	
+}
+
+function stageButtonAction(_){
+	
 }
 
 window.addEventListener('pagehide', _ => {
