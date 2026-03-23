59a2b3717dc1875960319d759314ea4283e59051
T.T. <196872942+TT-3-14159265358979323846264338@users.noreply.github.com>
2026-03-23 15:49:58 +0900

    Add files via upload

--- src/main/resources/static/js/status/status.js
+++ src/main/resources/static/js/status/status.js
@@ -1,4 +1,3 @@
-const returnButton = document.getElementById("go-to-original-from-status");
 export const weaponLink = "weapon";
 export const coreLink = "core";
 
@@ -6,17 +5,72 @@ export async function status(link, id){
 	try{
 		const response = await fetch(`/api/status/${link}/${id}`);
 		const data = await response.json();
-		
-		
-		
+		initialize(data);
 	}catch (e) {
 		console.error("ステータス画面の初期化失敗:", e);
 	}
 	getClassList().remove('hidden');
 }
 
+function initialize(data){
+	changeName(data.name);
+	changeWeapon(data.weaponElement, data.leftWeaponStatus, data.rightWeaponStatus);
+	changeUnit(data.unitElement, data.unitStatus)
+	changeCut(data.cutElement, data.cutStatus)
+	
+	
+	
+	
+	
+	
+}
+
+function changeName(name){
+	document.getElementById("element-name").textContent = name;
+}
+
+function changeWeapon(weaponElement, leftWeaponStatus, rightWeaponStatus){
+	const span = document.querySelectorAll("#weapon-status span");
+	span[0].textContent = "　";
+	for(let i = 1; i < 9; i++){
+		span[i].textContent = weaponElement[i - 1]? weaponElement[i - 1]: "　";
+	}
+	for(let i = 10; i < 18; i++){
+		span[i].textContent = leftWeaponStatus[i - 10]? leftWeaponStatus[i - 10]: "　";
+	}
+	if(rightWeaponStatus){
+		span[9].textContent = "左武器";
+		span[18].textContent = "右武器";
+		for(let i = 19; i < span.length; i++){
+			span[i].textContent = rightWeaponStatus[i - 19]? rightWeaponStatus[i - 19]: "　";
+		}
+	}else{
+		span[9].textContent = "武器性能";
+		span[18].textContent = "　";
+		for(let i = 19; i < span.length; i++){
+			span[i].textContent = "　";
+		}
+	}
+}
+
+function changeUnit(unitElement, unitStatus){
+	
+}
+
+function changeCut(cutElement, cutStatus){
+	
+}
+
+
+
+
+
+
+
+
+
 document.addEventListener('DOMContentLoaded', () => {
-	returnButton.addEventListener('click', returnButtonAction);
+	document.getElementById("go-to-original-from-status").addEventListener('click', returnButtonAction);
 });
 
 function returnButtonAction(){
