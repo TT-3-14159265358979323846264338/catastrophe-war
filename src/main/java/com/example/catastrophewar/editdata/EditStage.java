c02ac7b743f31453489c4f715cc2d322e0aa68af
T.T. <196872942+TT-3-14159265358979323846264338@users.noreply.github.com>
2026-04-14 07:32:04 +0900

    Add files via upload

--- src/main/java/com/example/catastrophewar/editdata/EditStage.java
+++ src/main/java/com/example/catastrophewar/editdata/EditStage.java
@@ -1,15 +1,18 @@
 package com.example.catastrophewar.editdata;
 
 import java.util.List;
+import java.util.stream.IntStream;
 
-import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.commonclass.ImageLink;
+import com.example.defaultdata.Stage;
 import com.example.savedata.InitializeSQL;
 import com.example.savedata.ItemRepository;
+import com.example.savedata.ItemSQL;
 import com.example.savedata.ProgressRepository;
 import com.example.savedata.ProgressSQL;
 
@@ -18,30 +21,56 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequiredArgsConstructor
 public class EditStage {
-	private final SimpMessagingTemplate messaging;
 	private final ItemRepository itemRepository;
 	private final ProgressRepository progressRepository;
 	
 	@GetMapping("/api/edit/stage/data")
 	@Transactional(readOnly = true)
 	public DefaultEditStageData sendEditStageData() {
-		List<ProgressSQL> progressSQL = progressRepository.findAll();
-		return new DefaultEditStageData(
-				medal(), 
-				ImageLink.stageNameList(), 
-				ImageLink.stageImageList(), 
-				progressSQL.stream().map(ProgressSQL::getStageClear).toList(), 
-				progressSQL.stream().map(ProgressSQL::getMerit).toList());
+		return new DefaultEditStageData(medalSQL().getNumber(), createStageData());
 	}
 	
-	record DefaultEditStageData(
-			int medal, 
-			List<String> stageName, 
-			List<String> stageImages, 
-			List<Boolean> stageClear, 
-			List<List<Boolean>> meritClear) {}
+	record DefaultEditStageData(int medal, List<StageData> stageData) {}
 	
-	int medal() {
-		return itemRepository.findById(InitializeSQL.MEDAL_INDEX).orElseThrow(() -> new RuntimeException("メダル数を取り込めない")).getNumber();
+	record StageData(String name, String image, boolean stageClear, List<Boolean> meritClear) {}
+	
+	ItemSQL medalSQL() {
+		return itemRepository.findById(InitializeSQL.MEDAL_INDEX).orElseThrow(() -> new RuntimeException("メダル数を取り込めない"));
+	}
+	
+	List<StageData> createStageData(){
+		Stage[] stage = Stage.values();
+		List<ProgressSQL> sql = progressRepository.findAll();
+		return IntStream.range(0, stage.length)
+				.mapToObj(i -> new StageData(stage[i].getLabel().getName(), 
+											stage[i].getLabel().getImageName(), 
+											sql.get(i).getStageClear(), 
+											sql.get(i).getMerit()))
+				.toList();
+	}
+	
+	@PostMapping("/api/edit/save/stage")
+	@Transactional
+	public void saveStageData(@RequestBody StageCondition stageCondition) {
+		saveMedal(stageCondition.medal);
+		saveClear(stageCondition.clear, stageCondition.merit);
+	}
+	
+	record StageCondition(int medal, List<Boolean> clear, List<List<Boolean>> merit) {}
+	
+	void saveMedal(int number) {
+		ItemSQL itemSQL = medalSQL();
+		itemSQL.setNumber(number);
+		itemRepository.save(itemSQL);
+	}
+	
+	void saveClear(List<Boolean> clear, List<List<Boolean>> merit) {
+		List<ProgressSQL> allSQL = progressRepository.findAll();
+		IntStream.range(0, clear.size()).forEach(i -> {
+			ProgressSQL sql = allSQL.get(i);
+			sql.setStageClear(clear.get(i));
+			sql.setData(merit.get(i));
+		});
+		progressRepository.saveAll(allSQL);
 	}
 }
--- src/main/java/com/example/catastrophewar/editdata/EditUnit.java
+++ src/main/java/com/example/catastrophewar/editdata/EditUnit.java
@@ -1,14 +1,15 @@
 package com.example.catastrophewar.editdata;
 
 import java.util.List;
+import java.util.stream.IntStream;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.example.commonclass.ImageLink;
 import com.example.defaultdata.Core;
 import com.example.defaultdata.DefaultData;
 import com.example.defaultdata.DefaultEnum;
@@ -22,35 +23,40 @@ import lombok.RequiredArgsConstructor;
 @RestController
 @RequiredArgsConstructor
 public class EditUnit {
-	private final SimpMessagingTemplate messaging;
 	private final CoreRepository coreRepository;
 	private final WeaponRepository weaponRepository;
 	
 	@GetMapping("/api/edit/unit/data")
 	@Transactional(readOnly = true)
 	public DefaultEditUnitData sendEditUnitData() {
-		return new DefaultEditUnitData(
-				nameList(Core.values()), 
-				nameList(Weapon.values()), 
-				ImageLink.normalCoreLinkList(), 
-				ImageLink.normalWeaponLinkList(), 
-				numberList(coreRepository), 
-				numberList(weaponRepository));
+		return new DefaultEditUnitData(createDataList(Core.values(), coreRepository), createDataList(Weapon.values(), weaponRepository));
 	}
 	
-	record DefaultEditUnitData(
-			List<String> coreName, 
-			List<String> weaponName, 
-			List<String> coreImages, 
-			List<String> weaponImages, 
-			List<Integer> coreNumber, 
-			List<Integer> weaponNumber) {}
+	record DefaultEditUnitData(List<UnitData> coreData, List<UnitData> weaponData) {}
 	
-	<T extends DefaultEnum<U>, U extends DefaultData> List<String> nameList(T[] data){
-		return DefaultEnum.getLabelStream(data).map(DefaultData::getName).toList();
+	record UnitData(String name, String image, Integer number) {}
+	
+	<T extends DefaultEnum<? extends DefaultData>, U extends JpaRepository<V, Integer>, V extends BaseSQL> List<UnitData> createDataList(T[] unit, U repository){
+		List<V> sql = repository.findAll();
+		return IntStream.range(0, unit.length).mapToObj(i -> createData(unit[i].getLabel(), sql.get(i))).toList();
 	}
 	
-	<T extends JpaRepository<? extends BaseSQL, Integer>> List<Integer> numberList(T repository){
-		return repository.findAll().stream().map(BaseSQL::getNumber).toList();
+	<T extends DefaultData, U extends BaseSQL> UnitData createData(T data, U sql) {
+		return new UnitData(data.getName(), data.getImageName(), sql.getNumber());
+	}
+	
+	@PostMapping("/api/edit/save/unit")
+	@Transactional
+	public void saveUnitData(@RequestBody UnitNumber unitNumber) {
+		saveData(unitNumber.coreNumber, coreRepository);
+		saveData(unitNumber.weaponNumber, weaponRepository);
+	}
+	
+	record UnitNumber(List<Integer> coreNumber, List<Integer> weaponNumber) {}
+	
+	<T extends JpaRepository<U, Integer>, U extends BaseSQL> void saveData(List<Integer> numberList, T repository) {
+		List<U> allSQL = repository.findAll();
+		IntStream.range(0, numberList.size()).forEach(i -> allSQL.get(i).setNumber(numberList.get(i)));
+		repository.saveAll(allSQL);
 	}
 }
